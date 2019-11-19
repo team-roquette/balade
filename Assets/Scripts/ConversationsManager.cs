c046ffffee79e7e2670b56d0016c03c4092002c5
Romain Dannin <romain.dannin@gmail.com>
2019-11-19 19:02:58 +0100

    better conversation manager for futur purposes

---
+++ Assets/Scripts/ConversationsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenConversation(string conversationName, Transform player, Transform conversant) {
+        if(PixelCrushers.DialogueSystem.DialogueManager.ConversationHasValidEntry(conversationName)) {
+            PixelCrushers.DialogueSystem.DialogueManager.StartConversation(conversationName, player, conversant);
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerBoyfriendComment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBoyfriendComment : MonoBehaviour
+{
+    private Transform player; 
+    private Transform boyfriend;
+    public string conversationName; 
+    public ConversationsManager conversationsManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        boyfriend = GameObject.Find("Boyfriend").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Boyfriend") {
+            conversationsManager.OpenConversation(conversationName, player, boyfriend);
+        }
+    }
+}
