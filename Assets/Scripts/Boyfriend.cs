14bda1cc066ea016dfff1090fa3d323f337611fe
Camille <camille.durif@edu.gobelins.fr>
2019-12-24 15:00:42 +0100

    AI Boyfriend (speed, stopping, lookat)
    Lookat to do better

--- Assets/Scripts/Boyfriend.cs
+++ Assets/Scripts/Boyfriend.cs
@@ -8,18 +8,70 @@ public class Boyfriend : MonoBehaviour
 
     public Transform goal;
 
+    NavMeshAgent agent;
+    bool updatePath = true;
+
     // Start is called before the first frame update
     void Start()
     {
 
-        
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(CalcPath());
+
+    }
+
+    IEnumerator CalcPath()
+    {
+        while (true)
+        {
+
+            updatePath = Vector3.Distance(transform.position, goal.position) < 6.0f ? false : true;
+
+            if (updatePath)
+            {
+                yield return new WaitForSeconds(0.2f);
+                agent.SetDestination(goal.position);
+            }
+            yield return null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+
+        if (Vector3.Distance(transform.position, goal.position) > 10.0f)
+        {
+            agent.speed = 7.0f;
+        }
+        else if(Vector3.Distance(transform.position, goal.position) < 7.0f)
+        {
+            agent.speed = 3.5f;
+        }
+
+
+        if (IsAtDestination())
+        {
+            transform.LookAt(goal.position);
+        }
+
+        //agent.destination = goal.position;
+    }
+
+    private bool IsAtDestination()
+    {
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
